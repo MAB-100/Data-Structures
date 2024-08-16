eb706dd4ffd23acd613ea0cddd7b9fda62893fdd
MAB-100 <shahabbas661@gmail.com>
2024-08-16 16:06:02 +0500

    week 3 done

--- LAB/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ LAB/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,20 +1,47 @@
 # python3
 
+
 from collections import namedtuple
 
-AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
+AssignedJob = namedtuple('AssignedJob', ['worker', 'started_at'])
 
 
-def assign_jobs(n_workers, jobs):
-    # TODO: replace this code with a faster algorithm.
-    result = []
-    next_free_time = [0] * n_workers
-    for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
+class JobQueue:
+    def __init__(self, n_workers, jobs):
+        self.n = n_workers
+        self.jobs = jobs
+        self.finish_time = []
+        self.assigned_jobs = []
+        for i in range(self.n):
+            self.finish_time.append([i, 0])
 
-    return result
+    def SiftDown(self, i):
+        min_index = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < self.n:
+            if self.finish_time[min_index][1] > self.finish_time[left][1]:
+                min_index = left
+            elif self.finish_time[min_index][1] == self.finish_time[left][1]:
+                if self.finish_time[min_index][0] > self.finish_time[left][0]:
+                    min_index = left
+        if right < self.n:
+            if self.finish_time[min_index][1] > self.finish_time[right][1]:
+                min_index = right
+            elif self.finish_time[min_index][1] == self.finish_time[right][1]:
+                if self.finish_time[min_index][0] > self.finish_time[right][0]:
+                    min_index = right
+        if min_index != i:
+            self.finish_time[i], self.finish_time[min_index] = self.finish_time[min_index], self.finish_time[i]
+            self.SiftDown(min_index)
+
+    def NextWorker(self, job):
+        root = self.finish_time[0]
+        next_worker = root[0]
+        started_at = root[1]
+        self.assigned_jobs.append(AssignedJob(next_worker,started_at))
+        self.finish_time[0][1] += job
+        self.SiftDown(0)
 
 
 def main():
@@ -22,7 +49,10 @@ def main():
     jobs = list(map(int, input().split()))
     assert len(jobs) == n_jobs
 
-    assigned_jobs = assign_jobs(n_workers, jobs)
+    job_queue = JobQueue(n_workers, jobs)
+    for job in jobs:
+        job_queue.NextWorker(job)
+    assigned_jobs = job_queue.assigned_jobs
 
     for job in assigned_jobs:
         print(job.worker, job.started_at)
--- LAB/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ LAB/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -1,40 +1,50 @@
 # python3
 
 
-class Database:
+class DataBases:
     def __init__(self, row_counts):
-        self.row_counts = row_counts
         self.max_row_count = max(row_counts)
+        self.row_counts = row_counts
         n_tables = len(row_counts)
-        self.ranks = [1] * n_tables
-        self.parents = list(range(n_tables))
+        self.parent = list(range(n_tables))
+        self.rank = [0] * n_tables
 
-    def merge(self, src, dst):
-        src_parent = self.get_parent(src)
-        dst_parent = self.get_parent(dst)
+    def FindParent(self, i):
+        if i != self.parent[i]:
+            self.parent[i] = self.FindParent(self.parent[i])
+        return self.parent[i]
 
+    def MergeTables(self, dst, src):
+        src_parent = self.FindParent(src)
+        dst_parent = self.FindParent(dst)
         if src_parent == dst_parent:
-            return False
-
-        # merge two components
-        # use union by rank heuristic
-        # update max_row_count with the new maximum table size
-        return True
-
-    def get_parent(self, table):
-        # find parent and compress path
-        return self.parents[table]
+            return
+        if self.rank[src_parent] > self.rank[dst_parent]:
+            self.parent[dst_parent] = src_parent
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+            self.row_counts[dst_parent] = 0
+            self.max_row_count = max(self.max_row_count, self.row_counts[src_parent])
+        else:
+            self.parent[src_parent] = dst_parent
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+            self.row_counts[src_parent] = 0
+            self.max_row_count = max(self.max_row_count, self.row_counts[dst_parent])
+            if self.rank[src_parent] == self.rank[dst_parent]:
+                self.rank[dst_parent] += 1
 
 
 def main():
     n_tables, n_queries = map(int, input().split())
     counts = list(map(int, input().split()))
-    assert len(counts) == n_tables
-    db = Database(counts)
+    assert(n_tables == len(counts))
+    db = DataBases(counts)
     for i in range(n_queries):
         dst, src = map(int, input().split())
-        db.merge(dst - 1, src - 1)
+        db.MergeTables(dst - 1, src - 1)
+        print('parent: ', db.parent)
+        print('row counts: ', db.row_counts)
         print(db.max_row_count)
+
 
 
 if __name__ == "__main__":
